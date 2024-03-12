e7a5ca8459fee02985ca5a9136e2ef7362091448
Jack Yang <jack@exacttechnology.com>
2024-03-12 01:09:15 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import img from './Q.jpg';
 
+
+document.title = 'Queering Christianity - Anxin Zhang';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header title='Hello'>
+        <img src={img} style={{ width: '100%' }} />
       </header>
     </div>
   );
